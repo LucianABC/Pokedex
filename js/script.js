e7fe544c1baeaa253bc066cbf9e1b02910076e21
LucianABC <louabecasis@gmail.com>
2020-01-14 12:03:25 -0300

    get pokemon

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 try {
     const variables = require("./variables.js");
-
     const baseUrl= variables.baseUrl;
 }catch(e){}
-const getPkm = async() => {
-    try {
 
+const input = document.querySelector("#pkm-search");
+const getPkm = async() => {
+    const id= input.value;
+    try {
+        const pokemon = await axios.get(`${baseUrl}/${id}`);
+        //get image
+        //get data
+        
     }catch(err){
 
     }
