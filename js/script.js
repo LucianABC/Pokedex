d8d528c8abcc18fd0c90263648d6c40ade3c4e70
LucianABC <louabecasis@gmail.com>
2020-01-14 12:39:13 -0300

    no se

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 try {
     const variables = require("./variables.js");
     const baseUrl= variables.baseUrl;
+    const handleError = variables.handleError;
+
 }catch(e){}
 
 const input = document.querySelector("#pkm-search");
+
 const getPkm = async() => {
     const id= input.value;
     try {
         const pokemon = await axios.get(`${baseUrl}/${id}`);
-        //get image
-        //get data
-        
     }catch(err){
 
     }
 }
+
+const submitSearch = document.querySelector("#submit-bttn");
+
+submitSearch.addEventListener("click", getPkm);
--- js/variables.js
+++ js/variables.js
@@ -1,12 +1,23 @@
-const baseUrl = "https://pokeapi.co/api/v2/";
+const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+const handleError = err =>{
+    alert(`Hubo un error. ${err}`);
+};
+const pokemonList = [];
 
+const getAllPokemon = async () => {
+    try {
+        const data = await axios.get(baseUrl);
+        pokemonList.push();
 
-
+    }catch(err){
+        handleError;
+    }
+}
 
 
 try {
     module.exports = {
         baseUrl,
-        
+        handleError
     }
 }catch(e){}
